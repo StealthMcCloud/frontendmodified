30f652c5330cde5406cde35c3157a7a324b9ac1a
Clinton Johnson <dragoon1246224@gmail.com>
2018-10-16 11:32:31 -0400

    Done

--- public/index.js
+++ public/index.js
@@ -12,7 +12,42 @@ function handleFormSubmit(event) {
         userName: document.getElementById("userName").value,
         fullName: document.getElementById("fullName").value,
         password: document.getElementById("password").value,
+        telephone: document.getElementById("telephone").value,
+        homepage: document.getElementById("homepage").value,
+        contactMethod: radioButtonChecker(),
+        preferredDevice: checkBoxChecker(),
+        userLevel: document.getElementById("level").value,
+        favoriteColor: document.getElementById("color").value,
     };
+
+    function radioButtonChecker() {
+        const email = document.getElementById("contactEmail").checked
+        const text = document.getElementById("contactText").checked
+        if (email === true) {
+            return "Email"
+        } else if (text === true) {
+            return "Text"
+        } 
+        return "Phone Call"
+    }
+
+    function checkBoxChecker() {
+        const Android = document.getElementById("Android").checked
+        const IOS = document.getElementById("IOS").checked
+        const Mac = document.getElementById("Mac").checked
+        const Windows = document.getElementById("Windows").checked
+        let preferredDevice = []
+        if (Android === true) {
+            preferredDevice.push("Android")
+        } if (IOS === true) {
+            preferredDevice.push("IOS")
+        } if (Mac === true) {
+            preferredDevice.push("Mac")
+        } if (Windows === true) {
+            preferredDevice.push("Windows")
+        } 
+        return preferredDevice
+    }
     
     fetch("/api/user", {
         method: "POST",
